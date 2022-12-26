e91f2cc5cc48c231a77edf0019191ccfbb3bc9b9
ehtsham-shahbaz-98 <ehtsham.shahbaz.1998@gmail.com>
2022-12-26 10:52:45 -0700

    update: navbar complete

---
+++ client/src/scenes/homePage/index.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/system";
+import Navbar from "../navbar/index";
+
+const HomePage = () => {
+  return (
+    <Box>
+      <Navbar />
+    </Box>
+  );
+};
+
+export default HomePage;
